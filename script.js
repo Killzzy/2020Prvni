9751332dda71265a75a5c7dcf841fe2ccaa2cd4c
killzzy <martin.bartos013@gmail.com>
2020-10-06 13:43:24 +0000

    Výběr operace a kontrola vstupů.

--- script.js
+++ script.js
@@ -8,7 +8,21 @@ function čus() {
 function vypocet() {
   let c1 = document.getElementById("cislo1").valueAsNumber;
   let c2 = document.getElementById("cislo2").valueAsNumber;
-  let vysl = c1 + c2;
-  console.log(vysl);
+  let op = document.getElementById("operace").value;
+  console.log(op);
 
+  let vysl = "?";
+  if (isNaN(c1)) {
+    alert("Není zadáno první číslo");
+  } else if (isNaN(c2)) {
+    alert ("Neni zadano druhe číslo");
+  } else if (op == "plus"){
+    vysl = c1 + c2;
+  } else if (op == "minus"){
+    vysl = c1 - c2;
+  }
+  console.log(vysl);
+  document.getElementById("vysledek").value = vysl;
+  document.getElementById("vysledek2").innerHTML = "Vysledek je <b>" + vysl + "</b>.";
+  
 }
