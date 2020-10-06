a2a75ac429efc793c6f3e1489a3de0564f4222ff
killzzy <martin.bartos013@gmail.com>
2020-10-06 13:13:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function čus() {
+  let jm = document.getElementById("jmeno").value;
+  //výpis do konzole
+  console.log(jm);
+  alert("Bonjour " + jm + "!");
+}
+
+function vypocet() {
+  let c1 = document.getElementById("cislo1").valueAsNumber;
+  let c2 = document.getElementById("cislo2").valueAsNumber;
+  let vysl = c1 + c2;
+  console.log(vysl);
+
+}
