c5883dd06335e5eadad484d4be72634ac7adb073
killzzy <martin.bartos013@gmail.com>
2020-10-06 14:23:30 +0000

    Násobení a dělení

--- script.js
+++ script.js
@@ -9,17 +9,23 @@ function vypocet() {
   let c1 = document.getElementById("cislo1").valueAsNumber;
   let c2 = document.getElementById("cislo2").valueAsNumber;
   let op = document.getElementById("operace").value;
-  console.log(op);
+  console.log(c1);
 
   let vysl = "?";
   if (isNaN(c1)) {
     alert("Není zadáno první číslo");
   } else if (isNaN(c2)) {
-    alert ("Neni zadano druhe číslo");
+    alert ("Neni zadáno druhé číslo");
   } else if (op == "plus"){
     vysl = c1 + c2;
   } else if (op == "minus"){
     vysl = c1 - c2;
+  } else if (op == "krat"){
+    vysl = c1 * c2;
+  } else if (c2 === 0) {
+    alert ("Nelze dělit nulou");
+  } else if (op == "deleno"){
+    vysl = c1 / c2;
   }
   console.log(vysl);
   document.getElementById("vysledek").value = vysl;
